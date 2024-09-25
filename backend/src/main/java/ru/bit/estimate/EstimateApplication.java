97e3e0c18e50b379d9821899a6edc5fdde9bc35e
daniil <pepethefrog27@mail.ru>
2024-09-25 20:28:42 +0500

    chore: initial commit

---
+++ backend/src/main/java/ru/bit/estimate/EstimateApplication.java
@@ -0,0 +1,13 @@
+package ru.bit.estimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimateApplication.class, args);
+	}
+
+}
