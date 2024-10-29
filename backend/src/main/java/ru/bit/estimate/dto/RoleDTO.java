b5686aad546e792b681d948c44560a76c5469203
daniil <pepethefrog27@mail.ru>
2024-10-29 19:23:50 +0500

    add: created role dto

---
+++ backend/src/main/java/ru/bit/estimate/dto/RoleDTO.java
@@ -0,0 +1,17 @@
+package ru.bit.estimate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDTO {
+    private UUID id;
+    private String name;
+}
