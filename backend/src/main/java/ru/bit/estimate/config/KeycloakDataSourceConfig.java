9d227c8136b4f429b8071474d4eb257492056550
daniil <pepethefrog27@mail.ru>
2024-11-12 14:30:01 +0500

    Add KeycloackDataSourceCfg

---
+++ backend/src/main/java/ru/bit/estimate/config/KeycloakDataSourceConfig.java
@@ -0,0 +1,63 @@
+package ru.bit.estimate.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "ru.bit.estimate.keycloak.repository",
+        entityManagerFactoryRef = "keycloakEntityManagerFactory",
+        transactionManagerRef = "keycloakTransactionManager"
+)
+public class KeycloakDataSourceConfig {
+
+    @Bean
+    public DataSourceProperties keycloakDataSourceProperties() {
+        DataSourceProperties properties = new DataSourceProperties();
+        properties.setUrl("jdbc:postgresql://keycloak_postgres:5432/keycloak");
+        properties.setUsername("postgres");
+        properties.setPassword("postgres");
+        properties.setDriverClassName("org.postgresql.Driver");
+        return properties;
+    }
+
+    @Bean
+    public DataSource keycloakDataSource() {
+        return keycloakDataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+    @Bean(name = "keycloakEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean keycloakEntityManagerFactory(
+            EntityManagerFactoryBuilder builder) {
+        return builder
+                .dataSource(keycloakDataSource())
+                .packages("ru.bit.estimate.keycloak.model")
+                .persistenceUnit("keycloak")
+                .properties(hibernateProperties())
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager keycloakTransactionManager(
+            EntityManagerFactoryBuilder builder) {
+        return new JpaTransactionManager(Objects.requireNonNull(keycloakEntityManagerFactory(builder).getObject()));
+    }
+
+    private Map<String, Object> hibernateProperties() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        return properties;
+    }
+
+}
