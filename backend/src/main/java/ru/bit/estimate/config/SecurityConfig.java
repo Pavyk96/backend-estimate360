926440d30fc3c5179bb9ac84541e4da27d21fa90
Антон Матушкин <matushkin-2017@mail.ru>
2024-11-18 10:58:33 +0500

    feat: добавлен bypass SecurityConfig

---
+++ backend/src/main/java/ru/bit/estimate/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package ru.bit.estimate.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Конфигурация SpringSecurity
+ * @author Matushkin Anton
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().permitAll());
+        return http.build();
+    }
+
+}
