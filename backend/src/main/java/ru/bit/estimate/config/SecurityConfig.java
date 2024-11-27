b41b5f90bdd19102292c4d66d356ae18e05823f7
Антон Матушкин <matushkin-2017@mail.ru>
2024-11-27 13:18:24 +0500

    refactor: обновлен securityConfig для тестов

--- backend/src/main/java/ru/bit/estimate/config/SecurityConfig.java
+++ backend/src/main/java/ru/bit/estimate/config/SecurityConfig.java
@@ -1,53 +1,93 @@
 package ru.bit.estimate.config;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.bit.estimate.config.filter.JWTAuthenticationFilter;
+import ru.bit.estimate.service.impl.AuthUserDetailsServiceImpl;
 
+/**
+ * Class that configures the security settings for the application.
+ * @author Matushkin Anton
+ */
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    @NonNull
+    private final AuthUserDetailsServiceImpl userDetailsService;
+
+    @NonNull
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    /**
+     * Configures the security filter chain.
+     * <p>
+     * @param httpSecurity the HTTP security configuration
+     * @return the configured SecurityFilterChain
+     * @throws Exception if an error occurs during configuration
+     */
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers(HttpMethod.GET, "api/questionnaires").permitAll()
-                                .requestMatchers(HttpMethod.GET, "api/questions").permitAll()
-
-                                .requestMatchers(HttpMethod.PUT, "api/questionnaires").hasRole("HR")
-                                .requestMatchers(HttpMethod.POST, "api/questionnaires").hasRole("HR")
-                                .requestMatchers(HttpMethod.DELETE, "api/questionnaires").hasRole("HR")
-
-                                .requestMatchers(HttpMethod.PUT, "api/questions").hasRole("HR")
-                                .requestMatchers(HttpMethod.POST, "api/questions").hasRole("HR")
-                                .requestMatchers(HttpMethod.DELETE, "api/questions").hasRole("HR")
-                                .anyRequest().permitAll()
-                ).sessionManagement(
-                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                ).oauth2ResourceServer(
-                        resourceServer -> resourceServer.jwt(
-                                jwtConvertor -> jwtConvertor.jwtAuthenticationConverter(
-                                    keycloakAuthConverter()
-                                )
-                        )
-                )
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
+                                .requestMatchers("/api/questions").hasAuthority("test-role")
+                                .requestMatchers("/api/records").hasAuthority("admin-role")
+                                .anyRequest().authenticated())
+                .sessionManagement(manager ->
+                        manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider()).addFilterBefore(
+                        jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
+                );
+        return httpSecurity.build();
     }
 
-    private Converter<Jwt,? extends AbstractAuthenticationToken> keycloakAuthConverter() {
-        var converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(
-                new AuthoritiesConverter()
-        );
-
-        return converter;
+    /**
+     * Configures the authentication provider.
+     * @return the configured AuthenticationProvider
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
     }
+
+    /**
+     * Configures the password encoder.
+     * @return the PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Configures the authentication manager.
+     * <p>
+     * @param authenticationConfiguration the authentication configuration
+     * @return the configured AuthenticationManager
+     * @throws Exception if an error occurs during configuration
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
